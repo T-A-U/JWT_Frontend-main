c8729409291c32881a5ab329aed97c5793a806ba
T-A-U <153134801+T-A-U@users.noreply.github.com>
2024-03-27 12:53:31 -0700

    cleanup

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -43,15 +43,16 @@ const StorePage = () => {
         allowfullscreen
       ></iframe><br></br>
              
-              <a href="/auth" className="rounded-md bg-indigo-600 px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
+
+            </div>
+          </div>
+          <a href="/auth" className="my-10 block rounded-full text-black bg-white px-3.5 py-2.5 text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600">
                 Login <span aria-hidden="true">→</span>
 
                 
               </a>
-
-            </div>
-          </div>
         </div>
+       
         <div
           className="absolute inset-x-0 top-[calc(100%-13rem)] -z-10 transform-gpu overflow-hidden blur-3xl sm:top-[calc(100%-30rem)]"
           aria-hidden="true"
